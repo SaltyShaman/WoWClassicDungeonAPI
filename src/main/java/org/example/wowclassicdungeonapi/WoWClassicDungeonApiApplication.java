67de7c7119bd60293e7cd1c7d93e0692f1364b87
Kristoffer Søholm Gillesberg <kristoffer6400@gmail.com>
2025-04-04 13:24:38 +0200

    Initial commit

---
+++ src/main/java/org/example/wowclassicdungeonapi/WoWClassicDungeonApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.wowclassicdungeonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoWClassicDungeonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoWClassicDungeonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/wowclassicdungeonapi/WoWClassicDungeonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wowclassicdungeonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoWClassicDungeonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
