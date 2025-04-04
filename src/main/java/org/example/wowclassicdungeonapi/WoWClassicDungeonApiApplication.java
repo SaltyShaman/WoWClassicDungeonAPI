c19f349faaac67a6f354341e647020eeae987105
Kristoffer Søholm Gillesberg <kristoffer6400@gmail.com>
2025-04-04 13:28:33 +0200

    packages

--- src/main/java/org/example/wowclassicdungeonapi/WoWClassicDungeonApiApplication.java
+++ src/main/java/org/example/wowclassicdungeonapi/WoWClassicDungeonApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WoWClassicDungeonApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WoWClassicDungeonApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WoWClassicDungeonApiApplication.class, args);
+    }
 
 }
--- src/test/java/org/example/wowclassicdungeonapi/WoWClassicDungeonApiApplicationTests.java
+++ src/test/java/org/example/wowclassicdungeonapi/WoWClassicDungeonApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WoWClassicDungeonApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
