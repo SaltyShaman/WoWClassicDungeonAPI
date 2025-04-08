44290c51191c0ef0d8b8ece1bf1550ace7c89fec
Lukas Balthazar <LBTurner@outlook.dk>
2025-04-08 10:26:08 +0200

    /v1/joke endpoint changed /v1/answer

--- src/main/java/org/example/wowclassicdungeonapi/controller/ChatGPTResponseController.java
+++ src/main/java/org/example/wowclassicdungeonapi/controller/ChatGPTResponseController.java
@@ -25,7 +25,7 @@ public class ChatGPTResponseController {
         this.openAIService = openAIService;
     }
 
-    @GetMapping("/v1/joke")
+    @GetMapping("/v1/answer")
     public MyResponse getAnswer(@RequestParam String about) {
         return openAIService.makeRequest(about, SYSTEM_MESSAGE);
     }
